8c913934ece447d30aafda058d1c60da7cec7ee1
marynmendes <marynicolesm@gmail.com>
2025-03-13 23:53:09 -0300

    Anotações referentes a aula de 10/03/2025

---
+++ aula_1/Empilhavel.java
@@ -0,0 +1,42 @@
+public interface Empilhavel {
+    //operações principais
+    void empilhar(Object dado); //push
+    Object desempilhar(); //pop
+    Object topo(); //top
+    void atualizar(Object dado); //update
+    
+    //operações auxiliares
+    Boolean estaCheia(); //isFull
+    Boolean estaVazia(); //isEmpty
+    String imprimir(); //print
+
+}
+
+/*CONJUNTO BÁSICO DE ESTRUTURA DE DADOS
+C- create (criar)
+R- read (ler)
+U- update (atualizar)
+D- delete (apagar)
+S- sort (ordenar)
+
+-----------------------------------------
+
+TIPOS DE DADOS
+- Numéricos
+    - inteiros
+        - unsigned int
+        - tinyint, smallint
+        - int
+        - long, longint, bigint
+    - ponto flutuante
+        - float
+        - double
+    - ponto fixo
+- String
+- Char
+- Lógico
+    - boolean, bool
+- Vetor, array, matriz unidimensional
+- Ponteiro
+- Objeto
+*/
