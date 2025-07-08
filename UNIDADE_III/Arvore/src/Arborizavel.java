a1a4af866fa7d783fe49fbc39cc13298ba1804b7
marynmendes <marynicolesm@gmail.com>
2025-07-08 18:26:59 -0300

    Anotações da aula do dia 30/06/2025

---
+++ UNIDADE_III/Arvore/src/Arborizavel.java
@@ -0,0 +1,10 @@
+public interface Arborizavel<T> {
+    NoTriplo<T> getRaiz();
+    void inserir(T dado);
+    T apagar(T dado);
+    boolean existe(T dado);
+    void limpar();
+    String imprimirPreOrdem();
+    String imprimirEmOrdem();
+    String imprimirPosOrdem();
+}
---
+++ UNIDADE_III/Arvore/src/NoTriplo.java
@@ -0,0 +1,36 @@
+public class NoTriplo<T> {
+    private T dado;
+    private NoTriplo<T> pai;
+    private NoTriplo<T> direita;
+    private NoTriplo<T> esquerda;
+
+    public NoTriplo(T dado) {
+        this.dado = dado;
+    }
+    
+    public T getDado() {
+        return dado;
+    }
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+    public NoTriplo<T> getPai() {
+        return pai;
+    }
+    public void setPai(NoTriplo<T> pai) {
+        this.pai = pai;
+    }
+    public NoTriplo<T> getDireita() {
+        return direita;
+    }
+    public void setDireita(NoTriplo<T> direita) {
+        this.direita = direita;
+    }
+    public NoTriplo<T> getEsquerda() {
+        return esquerda;
+    }
+    public void setEsquerda(NoTriplo<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+}
